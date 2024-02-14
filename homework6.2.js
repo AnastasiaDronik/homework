0e8784347e0ebc0fa1019d88c6b923a3efa38592
AnastasiaDronik <nastyahima@gmail.com>
2024-02-14 04:21:26 +0200

    added ; where needed

--- homework6.2.js
+++ homework6.2.js
@@ -4,11 +4,11 @@ function pow(x, y){
     let result = x;
     for (i=1; i<y; i++) {
 result = result*x
-    }
+    };
     if(y===0){
         result = 1;
     }
-    console.log(result)
+    console.log(result);
 }
 
 pow(3, 5)
